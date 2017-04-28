41849de817ddf8dcdb0468aeb6cb5a1f6a239f31
JaiGatsby <jais334@gmail.com>
2017-04-28 12:50:55 +0800

    Lecture notes and HW from Sung Kim's tutorial series

--- lab-11-1-mnist_cnn.py
+++ lab-11-1-mnist_cnn.py
@@ -13,26 +13,26 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # hyper parameters
 learning_rate = 0.001
-training_epochs = 15
+training_epochs = 1
 batch_size = 100
 
 # input place holders
 X = tf.placeholder(tf.float32, [None, 784])
-X_img = tf.reshape(X, [-1, 28, 28, 1])   # img 28x28x1 (black/white)
+X_img = tf.reshape(X, [-1, 28, 28, 1])   # img 28x28x1 (black/white), -1 represents we dont care about number of images
 Y = tf.placeholder(tf.float32, [None, 10])
 
 # L1 ImgIn shape=(?, 28, 28, 1)
-W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
+W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))# 3x3x1 with 5 filters, 1 refers to color
 #    Conv     -> (?, 28, 28, 32)
 #    Pool     -> (?, 14, 14, 32)
-L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
+L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')# output image size would be same
 L1 = tf.nn.relu(L1)
 L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1],
                     strides=[1, 2, 2, 1], padding='SAME')
 '''
-Tensor("Conv2D:0", shape=(?, 28, 28, 32), dtype=float32)
+Tensor("Conv2D:0", shape=(?, 28, 28, 32), dtype=float32) #32, coz we 32 filters
 Tensor("Relu:0", shape=(?, 28, 28, 32), dtype=float32)
-Tensor("MaxPool:0", shape=(?, 14, 14, 32), dtype=float32)
+Tensor("MaxPool:0", shape=(?, 14, 14, 32), dtype=float32) # as stride is 2x2, we get half size image
 '''
 
 # L2 ImgIn shape=(?, 14, 14, 32)
